3e9036cee1a4f076a608fbdba44f66a7460709dc
helga-hai <o.hainyzhnyk@gmail.com>
2019-08-24 10:24:40 +0300

    vanilla JS

--- js/app.js
+++ js/app.js
@@ -1,23 +1,20 @@
 //точка входа, контроллер, отлавливат события, навешивает обработчики
 //window.onload должен быть здесь, onEvents, запускает рендеры.
-//обращение к єлементам и получение данных
+//обращение к элементам и получение данных
 //контейнер куда вы будете рендерить вы здесь получаете. 
 //и говорите я ожидаю что то отсюда - и вам возвращается уже готовый темплейт
 //Если нужно передать что-то (списки, контейнеры) в модуль - передается параметрами, аргументами
-
-
 console.log('script.js')
-import Room from './generator.js'
-import Render from './render.js'
-
-const one = new Room;
+import itemRoom from './generator.js';
+import Render from './render.js';
 
 window.onload = function() {
-    document.body.innerHTML = Render.list([
-        '<strong>ROOM ' + one.number + '</strong>',
-        one.description(),
-        'aera: ' + one.aera + ' m<sup>2</sup>',
-        'levels: ' + one.levels,
-        'floor: ' + one.floor(),
-    ])
+
+
+    const listRooms = [new itemRoom, new itemRoom, new itemRoom, new itemRoom];
+
+    const roomsWrapper = document.querySelector('#rooms__wrapper');
+
+    Render.renderRooms(listRooms, roomsWrapper);
+
 }
